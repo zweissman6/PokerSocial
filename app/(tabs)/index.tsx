eb0642f5d4d313c515313fd751a366d693df9c2d
Zachary Weissman <zweissman6@gatech.edu>
2025-06-24 00:08:57 -0400

    first test

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -17,7 +17,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">PokerSocial Feed</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
