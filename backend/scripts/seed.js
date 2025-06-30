00c26c758e1e62a3311c5f4cfb5e33e7964c2a25
zweissman6 <zacharyweissman3@gmail.com>
2025-06-30 14:25:52 -0400

    moved script files

--- backend/scripts/seed.js
+++ backend/scripts/seed.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const User = require('./models/User');
-const Session = require('./models/Session');
+const User = require('../models/User');
+const Session = require('../models/Session');
 
 async function seed() {
   await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
